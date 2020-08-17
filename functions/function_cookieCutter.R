1d1f41edceeb7f1509d9da6805bc9684705c0f86
avisserquinn <a.visser-quinn@hw.ac.uk>
2020-08-17 12:17:07 +0100

    Overhaul of the cookieCutter function
    Function has been rewritten for speed using microbenchmark

--- functions/function_cookieCutter.R
+++ functions/function_cookieCutter.R
@@ -1,73 +1,28 @@
-cookieCutter <- function(dataInput, dataShapefile) { 
+cookieCutter <- function(dataInput, dataShapefile) {
   
-  source("functions/functions_internal.R", local = TRUE)
-  
-  # -------------------------------------------------------------------------
-  # STEP ONE - SETUP --------------------------------------------------------
-  # -------------------------------------------------------------------------
-  timeStamp <- paste0(Sys.time() %>% format("%H:%M:%S"), " ")
-  message(paste0(timeStamp, "Step one of three"))
-  # -------------------------------------------------------------------------
-  
-  pboptions(char = "=", type = "timer", style = 1)
-  
-  dt = dataInput %>% as_tibble %>% st_as_sf()
-  dtShp <- dataShapefile %>% summarise()
-  
-  
-  # -------------------------------------------------------------------------
-  # STEP TWO - st_join and st_contains --------------------------------------
-  # -------------------------------------------------------------------------
-  timeStamp <- paste0(Sys.time() %>% format("%H:%M:%S"), " ")
-  message(paste0(timeStamp, "Step two of three"))
-  # -------------------------------------------------------------------------
-  
-  dtJoin <- suppressMessages(dt %>% st_join(dtShp, left = FALSE))
-
-  dtContains <- suppressMessages(
-    dtJoin %>% 
-    mutate(contains = paste0("dl_", st_contains(dtShp, dtJoin, sparse = FALSE) %>% 
-                                       unlist %>% 
-                                       as.vector)) %>% 
-            split(., .$contains)
-  )
-  
-  
-  # -------------------------------------------------------------------------
-  # STEP THREE - TIDYING -----------------------------------------------------
-  # -------------------------------------------------------------------------  
-  timeStamp <- paste0(Sys.time() %>% format("%H:%M:%S"), " ")
-  message(paste0(timeStamp, "Step three of three"))
-  # -------------------------------------------------------------------------
-  
-  st3False <- dtContains[ which(str_detect(names(dtContains), "FALSE")) ] # Not contained within shapefile
-  st3True <- dtContains[ which(str_detect(names(dtContains), "TRUE")) ] # Contained within shapefile
-  
-  # Cut those objects which are not contained directly within the shapefile
-  st3False <- pblapply(st3False, function(x) { 
+  internalFunction <- function(dataInput, dataShapefile) { 
     
-    suppressMessages(
-      suppressWarnings( 
-        
-        x %>% st_make_valid %>% st_intersection(dtShp) 
-        
-      )) 
+    dataShapefile <- dataShapefile %>% summarise
     
-  })
+    index <- dataInput %>% st_intersects(dataShapefile) %>% lengths > 0 # Impacted by hazard
+    
+    cutout <- dataInput[index,] %>% st_intersection(dataShapefile) # Subset (cut)
+    
+    return(cutout)
+    
+  }
   
+  if(quiet == FALSE) { output <- internalFunction(dataInput, dataShapefile) }
+  if(quiet == TRUE) { output <- 
+    suppressWarnings( 
+      suppressMessages(
+        internalFunction(
+          dataInput, dataShapefile
+        )
+      )
+    )
+  }
   
-  # -------------------------------------------------------------------------
-  # PREPARING OUTPUT --------------------------------------------------------
-  # -------------------------------------------------------------------------
-  timeStamp <- paste0(Sys.time() %>% format("%H:%M:%S"), " ")
-  message(paste0(timeStamp, "Complete, preparing output"))
-  # -------------------------------------------------------------------------
-  
-  output <- c(st3False, st3True)
-  output <- output %>% modify(. %>% select(-contains)) %>% unname 
-  output <- output %>% mapedit:::combine_list_of_sf() %>% as_tibble %>% st_as_sf()
-  
-  class(output) <- c(class(output), "OSMtidy_cutOut")
   return(output)
   
 }
