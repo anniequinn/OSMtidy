8f134c6c4689c43d53cb9b08bc4400671a998e43
avisserquinn <a.visser-quinn@hw.ac.uk>
2020-08-17 13:25:00 +0100

    Overhaul of cookieCutter() for speed and efficiency

--- functions/function_cookieCutter.R
+++ functions/function_cookieCutter.R
@@ -1,52 +1,3 @@
-<<<<<<< Updated upstream
-cookieCutter <- function(dataInput, dataShapefile) { 
-  
-  source("functions/functions_internal.R", local = TRUE)
-  
-  # -------------------------------------------------------------------------
-  # STEP ONE - SETUP --------------------------------------------------------
-  # -------------------------------------------------------------------------
-  timeStamp <- paste0(Sys.time() %>% format("%H:%M:%S"), " ")
-  message(paste0(timeStamp, "Step one of three"))
-  # -------------------------------------------------------------------------
-  
-  pboptions(char = "=", type = "timer", style = 1)
-  
-  dt = dataInput %>% as_tibble %>% st_as_sf()
-  dtShp <- dataShapefile %>% summarise()
-  
-  
-  # -------------------------------------------------------------------------
-  # STEP TWO - st_join and st_contains --------------------------------------
-  # -------------------------------------------------------------------------
-  timeStamp <- paste0(Sys.time() %>% format("%H:%M:%S"), " ")
-  message(paste0(timeStamp, "Step two of three"))
-  # -------------------------------------------------------------------------
-  
-  dtJoin <- suppressMessages(dt %>% st_join(dtShp, left = FALSE))
-
-  dtContains <- suppressMessages(
-    dtJoin %>% 
-    mutate(contains = paste0("dl_", st_contains(dtShp, dtJoin, sparse = FALSE) %>% 
-                                       unlist %>% 
-                                       as.vector)) %>% 
-            split(., .$contains)
-  )
-  
-  
-  # -------------------------------------------------------------------------
-  # STEP THREE - TIDYING -----------------------------------------------------
-  # -------------------------------------------------------------------------  
-  timeStamp <- paste0(Sys.time() %>% format("%H:%M:%S"), " ")
-  message(paste0(timeStamp, "Step three of three"))
-  # -------------------------------------------------------------------------
-  
-  st3False <- dtContains[ which(str_detect(names(dtContains), "FALSE")) ] # Not contained within shapefile
-  st3True <- dtContains[ which(str_detect(names(dtContains), "TRUE")) ] # Contained within shapefile
-  
-  # Cut those objects which are not contained directly within the shapefile
-  st3False <- pblapply(st3False, function(x) { 
-=======
 cookieCutter <- function(sf, sfToCut, quiet = TRUE) {
   
   internalFunction <- function(sf, sfToCut) { 
@@ -56,7 +7,6 @@ cookieCutter <- function(sf, sfToCut, quiet = TRUE) {
     index <- sf %>% st_intersects(sfToCut) %>% lengths > 0 # Impacted by hazard
     
     cutout <- sf[index,] %>% st_intersection(sfToCut) # Subset (cut)
->>>>>>> Stashed changes
     
     suppressMessages(
       suppressWarnings( 
@@ -65,23 +15,9 @@ cookieCutter <- function(sf, sfToCut, quiet = TRUE) {
         
       )) 
     
-<<<<<<< Updated upstream
   })
   
-  
-  # -------------------------------------------------------------------------
-  # PREPARING OUTPUT --------------------------------------------------------
-  # -------------------------------------------------------------------------
-  timeStamp <- paste0(Sys.time() %>% format("%H:%M:%S"), " ")
-  message(paste0(timeStamp, "Complete, preparing output"))
-  # -------------------------------------------------------------------------
-  
-  output <- c(st3False, st3True)
-  output <- output %>% modify(. %>% select(-contains)) %>% unname 
-  output <- output %>% mapedit:::combine_list_of_sf() %>% as_tibble %>% st_as_sf()
-=======
-  }
-  
+
   if(quiet == FALSE) { output <- internalFunction(sf, sfToCut) }
   if(quiet == TRUE) { output <- 
     suppressWarnings( 
@@ -92,9 +28,7 @@ cookieCutter <- function(sf, sfToCut, quiet = TRUE) {
       )
     )
   }
->>>>>>> Stashed changes
-  
-  class(output) <- c(class(output), "OSMtidy_cutOut")
+
   return(output)
   
 }
