10f2e783aa64ef7d17fa1f8639b5b816155ab22a
avisserquinn <a.visser-quinn@hw.ac.uk>
2021-01-07 12:05:06 +0000

    Delete contents of OSMtidy folder
    Deleting all files from OSMtidy in preparation for the import of the OSMtidy packaged files

--- functions/functions.R
+++
@@ -1,9 +0,0 @@
-files <- list.files("functions", pattern = ".R", full.names = TRUE)
-
-files <- files[-which(files == "functions/functions.R")]
-files <- files[!str_detect(files, "internal")]
-files <- files[!str_detect(files, "tmp")]
-
-lapply(files, source)
-
-rm(files)
