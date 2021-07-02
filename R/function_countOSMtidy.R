95093ac170db0ba711bb094332f3942d8c5e8f8c
Melissa Bedinger <41333142+mbedinger@users.noreply.github.com>
2021-07-02 08:59:24 +0100

    Update countOSMtidy

--- R/function_countOSMtidy.R
+++ R/function_countOSMtidy.R
@@ -1,55 +1,82 @@
-countOSMtidy <- function(dg) {
-
-  # By category
+countOSMtidy <- function(dg, USAH = FALSE) {
   
-  count_category_type <- 
-    dg %>% 
-    countType(group = quo(category_mod)) %>%
-    rename(category = category_mod)
+  if(USAH = FALSE) {
+    
+    # By desc
+    
+    count_desc_type <-
+      dg %>% 
+      countType(group = quo(desc))
+    
+    count_desc_all <-
+      count_desc_type %>%
+      select(-type) %>%
+      group_by(desc) %>% 
+      summarise_each(funs(sum)) %>%
+      ungroup
+    
+    output <- 
+      list(count_desc_all, count_desc_type)
+    
+    names(output) <- 
+      c("byDesc_all", "byDesc_byType")
+    
+  }
   
-  count_category_all <-
-    count_category_type %>%
-    select(-type) %>%
-    group_by(category) %>% 
-    summarise_each(funs(sum)) %>%
-    ungroup
-  
-  
-  # By Urban Systems Abstraction Hierarchy physical object
-  
-  count_physicalObject_type <-
-    dg %>% countType(group = quo(physicalObject))
-  
-  count_physicalObject_all <-
-    count_physicalObject_type %>%
-    select(-type) %>%
-    group_by(physicalObject) %>% 
-    summarise_each(funs(sum)) %>%
-    ungroup
-  
-  
-  # By desc
-  
-  count_desc_type <-
-    dg %>% countType(group = quo(desc))
-  
-  count_desc_all <-
-    count_desc_type %>%
-    select(-type) %>%
-    group_by(desc) %>% 
-    summarise_each(funs(sum)) %>%
-    ungroup
-  
-  
-  output <- 
-    list(count_category_all, count_category_type, 
-         count_physicalObject_all, count_physicalObject_type, 
-         count_desc_all, count_desc_type)
-  
-  names(output) <- 
-    c("byCategory_all", "byCategory_byType", 
-      "byPhysicalObject_all", "byPhysicalObject_byType",
-      "byDesc_all", "byDesc_byType")
+  if(USAH = TRUE) {
+    
+    # By category
+    
+    count_category_type <- 
+      dg %>% 
+      countType(group = quo(category_mod)) %>%
+      rename(category = category_mod)
+    
+    count_category_all <-
+      count_category_type %>%
+      select(-type) %>%
+      group_by(category) %>% 
+      summarise_each(funs(sum)) %>%
+      ungroup
+    
+    
+    # By Urban Systems Abstraction Hierarchy physical object
+    
+    count_physicalObject_type <-
+      dg %>% countType(group = quo(physicalObject))
+    
+    count_physicalObject_all <-
+      count_physicalObject_type %>%
+      select(-type) %>%
+      group_by(physicalObject) %>% 
+      summarise_each(funs(sum)) %>%
+      ungroup
+    
+    
+    # By desc
+    
+    count_desc_type <-
+      dg %>% countType(group = quo(desc))
+    
+    count_desc_all <-
+      count_desc_type %>%
+      select(-type) %>%
+      group_by(desc) %>% 
+      summarise_each(funs(sum)) %>%
+      ungroup
+    
+    
+    output <- 
+      list(count_category_all, count_category_type, 
+           count_physicalObject_all, count_physicalObject_type, 
+           count_desc_all, count_desc_type)
+    
+    names(output) <- 
+      c("byCategory_all", "byCategory_byType", 
+        "byPhysicalObject_all", "byPhysicalObject_byType",
+        "byDesc_all", "byDesc_byType")
+    
+  }
   
   return(output)
   
--- R/function_countType.R
+++ R/function_countType.R
@@ -1,16 +1,40 @@
-countType <- function(dg, group) {
+countType <- function(dg, 
+                      group, 
+                      USAH = FALSE) {
   
-  output <-
-    dg %>%
-    as_tibble() %>%
-    mutate(type = st_geometry_type(geometry) %>% as.character,
-           area = st_area(geometry) %>% as.numeric,
-           length = st_length(geometry) %>% as.numeric) %>%
-    left_join(key, by = "desc") %>%
-    group_by(!!group, type) %>%
-    summarise(n = n(),
-              area = sum(area, na.rm = TRUE),
-              length = sum(length, na.rm = TRUE)) %>%
-    ungroup
+  if(USAH = FALSE) {
+    
+    output <-
+      dg %>%
+      as_tibble() %>%
+      mutate(type = st_geometry_type(geometry) %>% as.character,
+             area = st_area(geometry) %>% as.numeric,
+             length = st_length(geometry) %>% as.numeric) %>%
+      group_by(!!group, type) %>%
+      summarise(n = n(),
+                area = sum(area, na.rm = TRUE),
+                length = sum(length, na.rm = TRUE)) %>%
+      ungroup
+    
+  }
+  
+  if(USAH = TRUE) {
+    
+    output <-
+      dg %>%
+      as_tibble() %>%
+      mutate(type = st_geometry_type(geometry) %>% as.character,
+             area = st_area(geometry) %>% as.numeric,
+             length = st_length(geometry) %>% as.numeric) %>%
+      left_join(key, by = "desc") %>%
+      group_by(!!group, type) %>%
+      summarise(n = n(),
+                area = sum(area, na.rm = TRUE),
+                length = sum(length, na.rm = TRUE)) %>%
+      ungroup
+    
+  }
+  
+  return(output)
   
 }
