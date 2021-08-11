b63d117c4eab6cf8c0a55a732d7847ab64e26bbd
Melissa Bedinger <41333142+mbedinger@users.noreply.github.com>
2021-08-11 15:02:32 +0100

    Update cookieCutter function
    Updated cookieCutter function to ignore spherical coordinates until full review can be completed

--- R/function_cookieCutter.R
+++ R/function_cookieCutter.R
@@ -1,4 +1,12 @@
-cookieCutter <- function(sf, sfToCut, quiet = TRUE) {
+cookieCutter <- 
+  function(sf, 
+           sfToCut, 
+           quiet = TRUE) {
+  
+  # https://github.com/r-spatial/sf/issues/1649
+  # https://r-spatial.github.io/sf/articles/sf7.html
+  # sf::sf_use_s2()
+  sf::sf_use_s2(FALSE) # Until review for updated sf/s2 S^2 spherical space, revert to R^2 flat space
   
   internalFunction <- function(sf, sfToCut) {
     
@@ -23,4 +31,4 @@ cookieCutter <- function(sf, sfToCut, quiet = TRUE) {
   
   return(output)
   
-}
+  }
